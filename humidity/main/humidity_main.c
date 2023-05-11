386e5a978538736100a251964b9fdc5aded90351
faeiqkhan <65024753+faeiqkhan@users.noreply.github.com>
2023-05-12 02:32:10 +0530

    update

--- humidity/main/humidity_main.c
+++ humidity/main/humidity_main.c
@@ -6,13 +6,20 @@
 
 #include <esp_err.h>
 #include <dht/dht.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define DHT_GPIO 5 // D1 pin
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+#define DHT_GPIO 16 // D1 pin
+void setup()
+{
+    xMutex = xSemaphoreCreateMutex();
+}
 void temperature_task(void *arg)
 {
     ESP_ERROR_CHECK(dht_init(DHT_GPIO, false));
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
     while (1)
     {
         int humidity = 0;
@@ -25,11 +32,26 @@ void temperature_task(void *arg)
         } else {
             printf("Fail to get dht temperature data\n");
         }
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+    int temp = Temperature;
+    int humid = Humidity
     vTaskDelete(NULL);
 }
 
+void Output_Task(int temp,int humid)
+{
+ vTaskDelay(1000 / portTICK_PERIOD_MS)
+ while(1)
+ {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0,0); //col=0 row=0
+  lcd.print("Humidity: %d Temperature: %d\n", humid, temp);
+ }
+}
 void app_main() {
     xTaskCreate(temperature_task, "temperature task", 2048, NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(Output_Task, "Printer Task ", 2048, NULL, 2, NULL);
 }
